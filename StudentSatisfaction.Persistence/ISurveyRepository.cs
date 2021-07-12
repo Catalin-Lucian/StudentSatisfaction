ed3d2d85cbf70d782fdcf9173be82699ddd041b5
Carla <carla-adriana.morosan@student.tuiasi.ro>
2021-07-12 13:02:11 +0300

    creat ISurveyRepository, SurveyRepository

---
+++ StudentSatisfaction.Persistence/ISurveyRepository.cs
@@ -0,0 +1,19 @@
+﻿using StudentSatisfaction.Entities.Surveys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSatisfaction.Persistence
+{
+    public interface ISurveyRepository
+    {
+        IEnumerable<Survey> GetAll();
+        Task<Survey> GetSurveyById(Guid id);
+        Task Create(Survey survey);
+        void Update(Survey survey);
+        void Delete(Survey survey);
+        Task SaveChanges();
+    }
+}
---
+++ StudentSatisfaction.Persistence/SurveyRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSatisfaction.Entities.Surveys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSatisfaction.Persistence
+{
+    public class SurveyRepository : ISurveyRepository
+    {
+        private readonly SurveysContext _context;
+
+        public SurveyRepository(SurveysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(Survey survey)
+        {
+            await _context.Surveys.AddAsync(survey);
+        }
+
+        public void Update(Survey survey)
+        {
+            _context.Surveys.Update(survey);
+        }
+
+        public void Delete(Survey survey)
+        {
+            _context.Remove(survey);
+        }
+
+        public IEnumerable<Survey> GetAll()
+        {
+            return _context.Surveys;
+        }
+
+        public async Task<Survey> GetSurveyById(Guid id)
+        {
+            return await _context.Surveys
+                .Include(x => x.Topics)
+                .Include(x => x.Questions)
+                .Include(x => x.SubmittedQuestions)
+                .Include(x => x.Comments)
+                .Include(x => x.Users)
+                .FirstAsync(i => i.Id == id); 
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
