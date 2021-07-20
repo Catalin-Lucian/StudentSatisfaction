51c2187462a6613f1348d1ef63a3e3838c7351ec
Carla <carla-adriana.morosan@student.tuiasi.ro>
2021-07-20 21:11:54 +0300

    Test QuestionService GetById

--- StudentSatisfaction.Testing/QuestionServiceTest.cs
+++ StudentSatisfaction.Testing/QuestionServiceTest.cs
@@ -65,5 +65,41 @@ namespace StudentSatisfaction.Testing
             //Assert
             questions.Should().BeEquivalentTo(expectedResult);
         }
+
+        [Fact]
+        public async void When_GetById_IsCalled_Expect_QuestionWithSpecifiedId_ToBeReturned()
+        {
+            //Arrange
+            var survey = new Survey("Title", DateTime.Now, DateTime.Now.AddMonths(1));
+            var searchedId = Guid.NewGuid();
+
+            var q1 = new Question(searchedId, "plain text", "Question1");
+            var q2 = new Question(Guid.NewGuid(), "plain text", "Question2");
+
+            survey.Questions.Add(q1);
+            survey.Questions.Add(q2);
+
+            var expectedResult = new QuestionModel()
+            {
+                Id = q1.Id,
+                SurveyId = q1.SurveyId,
+                QuestionText = q1.QuestionText,
+                Type = q1.Type
+            };
+
+            _surveyRepositoryMock
+                .Setup(s => s.GetSurveyById(survey.Id))
+                .ReturnsAsync(survey);
+
+            _mapperMock
+                .Setup(m => m.Map<QuestionModel>(survey.Questions.FirstOrDefault(q => q.Id == searchedId)))
+                .Returns(expectedResult);
+
+            //Act
+            var questions = await _sut.GetById(survey.Id, searchedId);
+
+            //Assert
+            questions.Should().BeEquivalentTo(expectedResult);
+        }
     }
 }
